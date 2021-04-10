b1fb610a4dcc93d99bb644809a91da0a557c8cb6
Ilya Kreymer <ikreymer@gmail.com>
2021-04-09 21:28:42 -0700

    add missing file

---
+++ src/consts.js
@@ -0,0 +1,6 @@
+
+export const BEHAVIOR_WAIT_LOAD = "wait_load";
+export const BEHAVIOR_READY_START = "ready";
+export const BEHAVIOR_PAUSED = "paused";
+export const BEHAVIOR_RUNNING = "running";
+export const BEHAVIOR_DONE = "done";

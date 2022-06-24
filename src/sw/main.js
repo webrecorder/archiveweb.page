ceb53860a342600b815f85b7221575476faa7aa6
Ilya Kreymer <ikreymer@gmail.com>
2022-06-24 00:24:44 -0700

    readd rwp index.html

--- src/sw/main.js
+++ src/sw/main.js
@@ -4,7 +4,8 @@ import { ExtAPI } from "./api";
 
 import { RecordingCollections } from "./recproxy";
 
-import INDEX_HTML from "../../wr-ext/replay/index.html";
+import REC_INDEX_HTML from "../../wr-ext/replay/index.html";
+import RWP_INDEX_HTML from "replaywebpage/index.html";
 
 const defaultConfig = {
   injectScripts: ["/ruffle/ruffle.js"],
@@ -16,7 +17,9 @@ const staticData = new Map();
 
 const prefix = self.registration.scope;
 
-staticData.set(prefix + "record.html", {type: "text/html", content: INDEX_HTML});
+staticData.set(prefix, {type: "text/html", content: RWP_INDEX_HTML});
+staticData.set(prefix + "index.html", {type: "text/html", content: RWP_INDEX_HTML});
+staticData.set(prefix + "record.html", {type: "text/html", content: REC_INDEX_HTML});
 
 const useIPFS = false;
 const ApiClass = ExtAPI;

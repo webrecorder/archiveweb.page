4ba3b7153997bffeeb75cc3bb78e0aa718a07d04
emma <hi@emma.cafe>
2023-12-20 17:12:15 -0500

    add basic typescript configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,68 @@
+/* eslint-env node */
+/** @type { import("eslint").Linter.Config } */
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "webextensions": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-        "no-restricted-globals": [
-            2,
-            "event", "error"
-        ],
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  env: {
+    browser: true,
+    es6: true,
+    webextensions: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:lit/recommended",
+    "prettier",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  plugins: ["@typescript-eslint", "lit"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  rules: {
+    "no-restricted-globals": [2, "event", "error"],
+    "linebreak-style": ["error", "unix"],
+  },
+  ignorePatterns: [
+    "ruffle/**/*",
+    "build/**/*",
+    "/sw.js",
+    "/ui.js",
+    "dist/**/*",
+  ],
+  reportUnusedDisableDirectives: true,
 };
+
+// module.exports = {
+//     "env": {
+//         "browser": true,
+//         "es6": true,
+//         "webextensions": true
+//     },
+//     "extends": "eslint:recommended",
+//     "parserOptions": {
+//         "ecmaVersion": 2018,
+//         "sourceType": "module"
+//     },
+//     "rules": {
+//         "no-restricted-globals": [
+//             2,
+//             "event", "error"
+//         ],
+//         "indent": [
+//             "error",
+//             2
+//         ],
+//         "linebreak-style": [
+//             "error",
+//             "unix"
+//         ],
+//         "quotes": [
+//             "error",
+//             "double"
+//         ],
+//         "semi": [
+//             "error",
+//             "always"
+//         ]
+//     }
+// };

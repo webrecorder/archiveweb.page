b92e4c759b2b6e8dff634ecfc8b074cb22c55f67
emma <hi@emma.cafe>
2024-06-18 20:29:08 -0400

    fix ruffle downloader to only download latest with a selfhosted version

--- ruffle/download-latest-ruffle.sh
+++ ruffle/download-latest-ruffle.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 RUFFLE_DIR=$SCRIPT_DIR
 
-SELFHOST_URL=$(curl "https://api.github.com/repos/ruffle-rs/ruffle/releases" | jq -r '.[0].assets[] | select(.name | contains("selfhosted")) | .browser_download_url')
+SELFHOST_URL=$(curl "https://api.github.com/repos/ruffle-rs/ruffle/releases" | jq -r 'first(.[].assets[] | select(.name | contains("selfhosted"))) | .browser_download_url')
 
 echo "$SELFHOST_URL"
 

ee3a088eb84da3190c8975b1c4ee23b2c11cbbf2
Ilya Kreymer <ikreymer@users.noreply.github.com>
2025-12-05 16:26:05 -0800

    deps: update for 0.16.0
    deps: update dependencies for electron, replaywebpage, wabac.js (#332)
    cleanup: use globalThis instead of self

--- src/localstorage.ts
+++ src/localstorage.ts
@@ -1,17 +1,17 @@
 // @ts-expect-error - TS7006 - Parameter 'name' implicitly has an 'any' type. | TS7006 - Parameter 'value' implicitly has an 'any' type.
 export function setLocalOption(name, value) {
   // @ts-expect-error - TS2339 - Property 'chrome' does not exist on type 'Window & typeof globalThis'. | TS2339 - Property 'chrome' does not exist on type 'Window & typeof globalThis'.
-  if (self.chrome?.storage) {
+  if (globalThis.chrome?.storage) {
     return new Promise((resolve) => {
       const data = {};
       // @ts-expect-error - TS7053 - Element implicitly has an 'any' type because expression of type 'any' can't be used to index type '{}'.
       data[name] = value;
       // @ts-expect-error - TS2339 - Property 'chrome' does not exist on type 'Window & typeof globalThis'. | TS2794 - Expected 1 arguments, but got 0. Did you forget to include 'void' in your type argument to 'Promise'?
-      self.chrome.storage.local.set(data, () => resolve());
+      globalThis.chrome.storage.local.set(data, () => resolve());
     });
   }
 
-  if (self.localStorage) {
+  if (globalThis.localStorage) {
     return Promise.resolve(localStorage.setItem(name, value));
   }
 
@@ -21,16 +21,16 @@ export function setLocalOption(name, value) {
 // ===========================================================================
 export function getLocalOption(name: string): Promise<string | null> {
   // @ts-expect-error - TS2339 - Property 'chrome' does not exist on type 'Window & typeof globalThis'. | TS2339 - Property 'chrome' does not exist on type 'Window & typeof globalThis'.
-  if (self.chrome?.storage) {
+  if (globalThis.chrome?.storage) {
     return new Promise<string>((resolve) => {
       // @ts-expect-error - TS2339 - Property 'chrome' does not exist on type 'Window & typeof globalThis'.
-      self.chrome.storage.local.get(name, (res) => {
+      globalThis.chrome.storage.local.get(name, (res) => {
         resolve(res[name]);
       });
     });
   }
 
-  if (self.localStorage) {
+  if (globalThis.localStorage) {
     return Promise.resolve(localStorage.getItem(name));
   }
 
@@ -41,17 +41,17 @@ export function getLocalOption(name: string): Promise<string | null> {
 // @ts-expect-error - TS7006 - Parameter 'name' implicitly has an 'any' type.
 export function removeLocalOption(name) {
   // @ts-expect-error - TS2339 - Property 'chrome' does not exist on type 'Window & typeof globalThis'. | TS2339 - Property 'chrome' does not exist on type 'Window & typeof globalThis'.
-  if (self.chrome?.storage) {
+  if (globalThis.chrome?.storage) {
     return new Promise((resolve) => {
       // @ts-expect-error - TS2339 - Property 'chrome' does not exist on type 'Window & typeof globalThis'.
-      self.chrome.storage.local.remove(name, () => {
+      globalThis.chrome.storage.local.remove(name, () => {
         // @ts-expect-error - TS2794 - Expected 1 arguments, but got 0. Did you forget to include 'void' in your type argument to 'Promise'?
         resolve();
       });
     });
   }
 
-  if (self.localStorage) {
+  if (globalThis.localStorage) {
     return Promise.resolve(localStorage.removeItem(name));
   }
 

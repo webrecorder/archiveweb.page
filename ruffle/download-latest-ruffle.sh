4bfe7017c26d2074e6eb05619d74e5a7dd01833d
Ilya Kreymer <ikreymer@gmail.com>
2026-08-21 11:54:01 -0700

    ci: update to use new azure code signing to match RWP add force_release to test builds for 0.17.0-beta.0

--- ruffle/download-latest-ruffle.sh
+++ ruffle/download-latest-ruffle.sh
@@ -3,15 +3,19 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 RUFFLE_DIR=$SCRIPT_DIR
 
-SELFHOST_URL=$(curl -H "Authorization: ${AUTH_TOKEN}" "https://api.github.com/repos/ruffle-rs/ruffle/releases" | jq -r 'first(.[].assets[] | select(.name | contains("selfhosted"))) | .browser_download_url')
+if [ -z "$GH_TOKEN" ]; then
+  SELFHOST_URL=$(curl -s "https://api.github.com/repos/ruffle-rs/ruffle/releases" | jq -r '.[0].assets[] | select(.name | contains("selfhosted")) | .browser_download_url')
+else
+  SELFHOST_URL=$(curl -s --header "Authorization: Bearer $GH_TOKEN" "https://api.github.com/repos/ruffle-rs/ruffle/releases" | jq -r '.[0].assets[] | select(.name | contains("selfhosted")) | .browser_download_url')
+fi
 
 echo "$SELFHOST_URL"
 
-curl -L -o $RUFFLE_DIR/ruffle.zip "$SELFHOST_URL"
+curl -L -o "$RUFFLE_DIR/ruffle.zip" "$SELFHOST_URL"
 
-rm $RUFFLE_DIR/*.js $RUFFLE_DIR/*.wasm
+rm "$RUFFLE_DIR/*.js" "$RUFFLE_DIR/*.wasm"
 
-cd $RUFFLE_DIR
-unzip $RUFFLE_DIR/ruffle.zip *.js *.wasm
+cd "$RUFFLE_DIR" || exit 1
+unzip "$RUFFLE_DIR/ruffle.zip" "*.js" "*.wasm"
 
-rm $RUFFLE_DIR/ruffle.zip
+rm "$RUFFLE_DIR/ruffle.zip"
